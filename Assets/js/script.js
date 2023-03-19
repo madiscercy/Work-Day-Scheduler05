58b79b43bb60d872494a769a8398d9bb96f8805f
madiscercy <madiscercy@gmail.com>
2023-03-18 20:42:35 -0400

    added local storage to every hour

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -29,10 +29,6 @@ $(function () {
     $(".time-block").each(function() {
       const blockId = $(this).attr("id");
       const blockTime = parseInt(blockId.replace('hour-',''));
-      console.log("BLOCKTIME")
-      console.log(blockTime);
-      console.log("CURRENTIME")
-      console.log(currentTime);
       if (currentTime > blockTime) {
         $(this).addClass("past");
       } else if (currentTime === blockTime) {
@@ -48,6 +44,15 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  
 
   // TODO: Add code to display the current date in the header of the page.
   $("#currentDay").text(dayjs().format('MM/DD/YYYY'));
